877f6872e0cbe006217fe0e10ccf7d4b29f8a8b6
asia <asia.skoromna@gmail.com>
2026-05-31 15:35:29 +0300

    feat: implement notes CRUD with MongoDB, routes, middleware and deployment setup

---
+++ src/controllers/notesController.js
@@ -0,0 +1,48 @@
+import { Notes } from "../models/note";
+import createHttpError from "http-errors";
+
+
+
+export const getAllNotes = async (req, res) => {
+  const notes = await Notes.find();
+  res.status(200).json(notes);
+};//повертає всі нотатки GET
+
+export const getNoteById = async(req,res) => {
+  const { noteId } = req.params;
+  const notes = await Notes.findOne({ _id: noteId });
+
+  if (!notes) {
+    throw createHttpError(404, 'Note not found');
+  }
+  res.status(200).json(notes);
+};//повертає одну нотатку GET
+
+export const createNote = async (req, res) => {
+  const notes = await Notes.create(req.body);
+  res.status(201).json(notes);
+};//створення нотатки POST
+
+export const deleteNote = async (req, res) => {
+  const { noteId } = req.params;
+  const notes = await Notes.findOneAndDelete({ _id: noteId, });
+  if (!notes) {
+    throw createHttpError(404, 'Note not found');
+
+  }
+  res.status(200).json(notes);
+};//видалення через айді DELETE
+
+
+export const updateNote = async (req, res) => {
+  const { noteId } = req.params;
+  const notes = await Notes.findByIdAndUpdate(
+    { _id: noteId },
+    req.body,
+    { returnDocument: "after" },
+  );
+  if (!notes) {
+    throw createHttpError(404, 'Note not found');
+  }
+  res.status(200).json(notes);
+};//оновлення PATCH
---
+++ src/models/note.js
@@ -0,0 +1,27 @@
+import { model, Schema } from "mongoose";
+
+
+const notesSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  content: {
+    type: String,
+    default: "",
+    trim: true
+  },
+  tag: {
+    type: String,
+    enum: ['Work', 'Personal', 'Meeting', 'Shopping', 'Ideas', 'Travel', 'Finance', 'Health', 'Important', 'Todo'],
+    default: 'Todo'
+  },
+
+},
+ {
+    timestamps: true,
+    versionKey: false
+  }
+);
+export const Notes = model('Notes', notesSchema);
